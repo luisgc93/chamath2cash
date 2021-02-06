9901994fa4db8b9870b86f55c9bd234d6e0305fb
Luis Garcia Cuesta <luisgc93@gmail.com>
2021-02-06 23:40:47 +0100

    update run method

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,11 @@
+import re
+from datetime import datetime, timedelta
 from os import environ
 
 import tweepy
 import alpaca_trade_api as tradeapi
+
+from constants import CASHTAG, CHAMATH_TW_ID
 
 
 def init_tweepy():
@@ -10,48 +14,33 @@ def init_tweepy():
     return tweepy.API(auth)
 
 
-def buy(ticker):
-    api = tradeapi.REST(
+def init_alpaca():
+    return tradeapi.REST(
         key_id=environ["APCA_API_KEY_ID"],
         secret_key=environ["APCA_SECRET_KEY"],
         base_url=environ["APCA_BASE_URL"]
-    )
-
-    api.submit_order(
-        symbol=ticker,
-        qty=1,
-        side='buy',
-        type='market',
-        time_in_force='gtc'
-    )
-
-
-def sell(ticker):
-    api = tradeapi.REST(
-        key_id=environ["APCA_API_KEY_ID"],
-        secret_key=environ["APCA_SECRET_KEY"],
-        base_url=environ["APCA_BASE_URL"]
-    )
-
-    api.submit_order(
-        symbol=ticker,
-        qty=1,
-        side='sell',
-        type='market',
-        time_in_force='gtc'
     )
 
 
 def run():
     api = init_tweepy()
-    statuses = api.user_timeline(screen_name="chamath", count=1)
-    json_tweets = [status._json for status in statuses if status.in_reply_to_status_id is None]
-    text = json_tweets[0]["text"]
-    stocks = get_stocks(text)
-
-    if stocks:
-        api.update_status(status=f"Chamath recommends buying {stocks[0]}")
+    statuses = api.user_timeline(user_id=CHAMATH_TW_ID, count=5)
+    for status in statuses:
+        stocks = get_stocks(status.text)
+        if not stocks:
+            continue
+        for stock in stocks:
+            if datetime.now() - timedelta(minutes=2) <= status.created_at:
+                api.update_status(status=f"Buying ${stock} 🚀")
+                init_alpaca().submit_order(
+                    symbol=stock,
+                    qty=1,
+                    side='buy',
+                    type='market',
+                    time_in_force='gtc'
+                )
 
 
 def get_stocks(tweet):
-    return ["TSLA"]
+    if CASHTAG in tweet:
+        return re.findall(r"[$][A-Za-z]*", tweet)
